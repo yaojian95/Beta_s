2680760a1cdd8e3808f139786242e34cd47ff074
辣条 <1427456796@qq.com>
2021-08-14 11:18:57 -0700

    expand prior of dynesty, give up time-out decorator

--- Test_exe_file_mpi.py
+++ Test_exe_file_mpi.py
@@ -1,6 +1,8 @@
 from scipy.stats import rice
 from scipy.stats.mstats import gmean
 
+import timeout_decorator 
+
 import time
 import numpy as np
 
@@ -48,64 +50,76 @@ As = np.ones(npix); betas = np.ones(npix)
 ## import data
 
 total_P = np.load('/global/cscratch1/sd/jianyao/CBASS/Observations/homo_noise/totalP_s0_%s_uK_RJ_000.npy'%Nside)
-# P_nu0 = np.load('/global/cscratch1/sd/jianyao/CBASS/Foreground/P_nu0_s0_%s_uK_RJ.npy'%Nside)
 
 total_sigma = np.load('/global/cscratch1/sd/jianyao/CBASS/Noise/homo_noise/5_fre_sigma_P_%s_uK_RJ.npy'%Nside)
 masked_index = np.load('/global/cscratch1/sd/jianyao/CBASS/masked_index.npy')
 npara = 2; 
 
 def prior(cube):
-    A0 = cube[0]*200 # 0-100
+    A0 = cube[0]*1000 # 0-100
     beta = cube[1]*2 - 4
     
     return [A0, beta]
 
-def log_run(subset_pixels):
-#     start = time.time()
-    As = []; betas = []
-    for n in subset_pixels:
-        start = time.time()
-        logL = Loglikeli.logLike(Nside, fres,fres_list, total_P, total_sigma, n) ### need to be put outside the loop
-
-        sampler = dynesty.NestedSampler(logL.loglike, prior, npara, nlive=200, bootstrap = 0)
-        sampler.run_nested(dlogz = 0.1, print_progress=False) #
-        results = sampler.results
-
-        samples, weights = results.samples, np.exp(results.logwt - results.logz[-1])
-
-        As.append(quantile(samples[:,0], [0.5], weights)[0])
-        betas.append(quantile(samples[:,1], [0.5], weights)[0])
-
-        if n%20 == 0:
-            end = time.time()
-            cost = (end - start)/60
-            print('Time cost is %s mins for pixel %s at rank %s'%(cost, n, rank))
-        
-    #         print(n, As, betas)
-    return np.array((As, betas))
-
+logL = Loglikeli.logLike(Nside, fres,fres_list, total_P, total_sigma, 0)
+ 
+# @timeout_decorator.timeout(90) 
+def log_run(logL, index):
+    
+    logL.index = index
+    sampler = dynesty.NestedSampler(logL.loglike, prior, npara, nlive=200, bootstrap = 0)
+    sampler.run_nested(dlogz = 0.1, print_progress=False) #
+    results = sampler.results
+    
+    samples, weights = results.samples, np.exp(results.logwt - results.logz[-1])
+    
+    As = quantile(samples[:,0], [0.5], weights)[0];
+    beta_s = quantile(samples[:,1], [0.5], weights)[0]
+    
+    return np.array((As, beta_s))
+    
 ## 45 ranks, 1755 pixels for the masked region, each rank has 39 pixels
 
 N = int(args.npix) #int(len(masked_index)/size) ## size = 45
-subset = masked_index[np.arange((rank)*N, (rank+1)*N)]
 
-sendbuf = log_run(subset)
+subset_pixels = masked_index[np.arange((rank)*N, (rank+1)*N)]    
 
+paras = np.zeros((N, 2))
+j = 0
+
+start = time.time()
+for n in subset_pixels:
+    
+#     try: 
+    paras[j] = log_run(logL, n)
+    j += 1
+                     
+#     except TimeoutError:
+#         end = time.time()
+#         cost = (end - start)/60
+#         print('pixel %s takes too long - %s mins.'%(n, cost))
+#         j += 1
+#         continue 
+                     
 # print(As_beta)
 
+sendbuf = paras
+
+# print(sendbuf.shape)
 recvbuf = None
 if rank == 0:
-    recvbuf = np.ones((size,2*N))  
+    recvbuf = np.ones(size*2*N, dtype='d')
+#     print(recvbuf.shape)
 
 comm.Gather(sendbuf, recvbuf, root=0)
 
 if rank == 0:
     if args.frelist == 'spass_only':
-        np.save('/global/cscratch1/sd/jianyao/CBASS/Results/s0_only_homo_noise/As_betas_masked_both_32_with_SPASS_only.npy', recvbuf)
+        np.save('/global/cscratch1/sd/jianyao/CBASS/Results/s0_only_homo_noise/Dyne_As_betas_masked_both_32_with_SPASS_only.npy', recvbuf)
     if args.frelist == 'cbass_only':
-        np.save('/global/cscratch1/sd/jianyao/CBASS/Results/s0_only_homo_noise/As_betas_masked_both_32_with_CBASS_only_2.npy', recvbuf)
+        np.save('/global/cscratch1/sd/jianyao/CBASS/Results/s0_only_homo_noise/Dyne_As_betas_masked_both_32_with_CBASS_only_2.npy', recvbuf)
     if args.frelist == 'both':
-        np.save('/global/cscratch1/sd/jianyao/CBASS/Results/s0_only_homo_noise/As_betas_masked_both_32_with_SPASS_CBASS.npy', recvbuf)
+        np.save('/global/cscratch1/sd/jianyao/CBASS/Results/s0_only_homo_noise/Dyne_As_betas_masked_both_32_with_SPASS_CBASS.npy', recvbuf)
     
     end = time.time()
     print('Time cost is %s mins.'%((end-start)/60))
